b062be9587aa0a7ab974ff393c5a7b7409da492e
Alex Rakowski <alex.g.rakowski@gmail.com>
2021-04-18 15:51:23 +0100

    Add Seldon Core setup to run script

--- run.sh
+++ run.sh
@@ -1,10 +1,14 @@
 #!/usr/bin/env bash
 
 cluster_name='seldon-test'
+ns_name='seldon-system'
 
 # Set up environment
 kind create cluster --name $cluster_name
-kubectl create namespace seldon-system
+kubectl create namespace $ns_name
+helm install seldon-core seldon-core-operator \
+  --namespace $ns_name \
+  --repo https://storage.googleapis.com/seldon-charts
 
 # Run test
 
