9dbd3e4462a481469b191b3280bce4cc6b160d40
Alex Rakowski <alex.g.rakowski@gmail.com>
2021-04-18 19:37:37 +0100

    Create Seldon MLv1 client

--- main.go
+++ main.go
@@ -2,9 +2,28 @@ package main
 
 import (
   "fmt"
+  "os"
+  "path/filepath"
+
+  seldon "github.com/seldonio/seldon-core/operator/client/machinelearning.seldon.io/v1/clientset/versioned"
+  "k8s.io/client-go/tools/clientcmd"
 )
 
 func main() {
-  fmt.Println("Hello world")
+  kubeconfigPath := filepath.Join(os.Getenv("HOME"), ".kube", "config")
+
+  config, err := clientcmd.BuildConfigFromFlags("", kubeconfigPath)
+  if err != nil {
+    panic(err)
+  }
+
+  clientset, err := seldon.NewForConfig(config)
+  if err != nil {
+    panic(err)
+  }
+
+  clientset.MachinelearningV1().SeldonDeployments("seldon")
+
+  fmt.Println("Created Seldon client")
 }
 
